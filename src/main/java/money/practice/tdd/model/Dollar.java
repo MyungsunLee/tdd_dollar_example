71fdb097fa03f845378797776882903428a8f473
MyungsunLee <naru101@naver.com>
2021-02-22 23:04:35 +0900

    times(multiply) test2, change return type of times method

--- src/main/java/money/practice/tdd/model/Dollar.java
+++ src/main/java/money/practice/tdd/model/Dollar.java
@@ -2,14 +2,14 @@ package money.practice.tdd.model;
 
 public class Dollar {
 
-    private int amount;
+    public int amount;
 
     public Dollar(int money){
         this.amount = money;
     };
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount*multiplier);
     }
 
     public int getAmount(){
--- src/test/java/money/practice/tdd/currencyTdd/TestCurrency.java
+++ src/test/java/money/practice/tdd/currencyTdd/TestCurrency.java
@@ -10,11 +10,12 @@ public class TestCurrency {
     public void testMultiplication() {
 
         Dollar five = new Dollar(5);
-        five.times(2);
 
-        assertEquals(10,five.getAmount());
+        Dollar product = five.times(2);
+        assertEquals(10,product.amount);
 
-
+        product = five.times(3);
+        assertEquals(15,product.amount);
 
     }
 
